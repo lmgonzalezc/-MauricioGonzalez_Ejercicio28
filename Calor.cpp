6b8d91c722813a96a400fd4bcd9378b1ab36dfcc
Mauricio González <46940813+lmgonzalezc@users.noreply.github.com>
2019-05-02 07:53:23 -0500

    Calor.cpp

--- Calor.cpp
+++ Calor.cpp
@@ -14,9 +14,7 @@ int main(){
   double c = 900.0;
   double rho = 2700;
   int i, j;
-  int it=0;
   ofstream outfile;
-  double err=10;
 
   for(i=0;i<nx;i++){
     for(j=0;j<nt;j++){
@@ -31,38 +29,25 @@ int main(){
       }
     }
   }
-
-  while(err>1E-7){
-    it +=1;
-    cout << it << endl;
-    for(i=1;i<nx-1;i++){
-      for(j=1;j<nt-1;j++){
-        un[i][j] = uo[i][j-1]+ ((dt*k)/(pow(dx,2)*c*rho))*(uo[i+1][j]+uo[i-1][j] - 1*uo[i][j-1]);
-      }
-    }
-
-    for(i=0;i<nx;i++){
-      for(j=0;j<nt;j++){	
-        if((i==0) || (i=200)){
-          un[i][j] = 300;
-        }
-      }
-    }
-    
-    err = 0.0;
-    for(i=1;i<nx-1;i++){
-      for(j=1;j<nt-1;j++){
-        err += abs(uo[i][j]-un[i][j]);
-      }
-    }
-    for(i=1;i<nx-1;i++){
-      for(j=1;j<nt-1;j++){
-        uo[i][j] = un[i][j];
-      }
+  for(i=1;i<nx-1;i++){
+    for(j=1;j<nt-1;j++){
+      un[i][j] = uo[i][j-1]+ ((dt*k)/(pow(dx,2)*c*rho))*(uo[i+1][j]+uo[i-1][j] - 1*uo[i][j-1]);
     }
   }
 
-
+  for(i=0;i<nx;i++){
+    for(j=0;j<nt;j++){	
+      if((i==0) || (i=200)){
+        un[i][j] = 300;
+      }
+    }
+  }
+    
+  for(i=1;i<nx-1;i++){
+    for(j=1;j<nt-1;j++){
+      uo[i][j] = un[i][j];
+    }
+  }
   outfile.open("Calor.dat");
   for(j=0;j<nx;j++){
     for(i=0;i<nt;i++){
